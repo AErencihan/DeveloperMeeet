bb8deeff90fd865ec3b436dadcfec4357ea9a4a1
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-12-07 10:26:31 +0300

    comment line add

---
+++ init.sh
@@ -0,0 +1,6 @@
+
+php artisan make:migration 2014_10_12_000000_create_users_table.php
+php artisan make:migration 2014_10_12_100000_create_activity_registrations_table.php
+php artisan make:migration 2019_08_19_000000_create_activity_table.php
+php artisan make:migration 2019_12_14_000001_create_comments_table.php
+php artisan make:migration 2019_12_14_000001_create_form_component_table.php
