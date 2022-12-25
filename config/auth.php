7e5d69e79dc19a80d0142c1084dbb1930cdfc9c7
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-12-25 16:22:24 +0300

    comment line add

--- config/auth.php
+++ config/auth.php
@@ -62,7 +62,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Models\User::class,
+            'model' => \App\Http\Controllers\User::class,
         ],
 
         // 'users' => [
