780babacb8a37c23ccbf63b9d4147e4ad0bfce65
furkanozmen <ozmenf97@gmail.com>
2023-04-20 01:56:29 +0300

    new dashboard.php

--- app/Http/Controllers/activity/ProfileService.php
+++ app/Http/Controllers/activity/ProfileService.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\activity;
 
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\SessionUtil;
 
 class ProfileService extends Controller
 {
     public function viewProfile()
     {
-        return view('auth.profile');
+        $user = SessionUtil::getUser();
+        if ($user == null) {
+            return redirect('/login');
+        }
+        $activities = Activity::where('user_id', $user->id)->get();
+        return view('auth.profile', compact('user', 'activities'));
     }
-
 }
